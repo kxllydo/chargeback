71257e95d60fe8d5bf6a3fc35483d1f3ca3aeff6
Shannon <shannon-steven@aramark.com>
2024-04-16 14:26:36 -0400

    added main.py : csv->json

---
+++ main.py
@@ -0,0 +1,37 @@
+import csv
+import json
+
+def csv_to_json(csvf, jsonf):
+    '''
+        Pre-processing of data to clean up irrelevant fields.
+        Only relevant fields are: Resource Group, Cost, and Tags.
+
+        Args:
+            csvf (string)   : Path to .CSV file to be read
+            jsonf (string)  : Path to .JSON file to be written
+
+        Returns:
+            jsonf
+    '''
+
+    with open(csvf, 'r', encoding = 'utf-8') as csvf_o:
+        with open(jsonf, 'w', encoding = 'utf-8') as jsonf_o:
+            json_array = []
+            csvr = csv.DictReader(csvf_o)
+
+            for row in csvr:
+                new_row = {}
+                new_row['RG'] = row['ResourceGroup']
+                new_row['Cost'] = row['Cost']
+                #new_row['Tags'] = row['Tags']
+
+                json_array.append(new_row)
+
+            jsonString = json.dumps(json_array, indent = 4)
+            jsonf_o.write(jsonString)
+
+if __name__ == "__main__":
+    csv_file = r"./billing.csv"
+    json_file = r"./resource-group-costs-json.txt"
+
+    csv_to_json(csv_file, json_file)
