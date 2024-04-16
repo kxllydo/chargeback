df7bd0f80434bc23b5789a62278ab37a36f23923
Kelly Do <kellydo8467@gmail.com>
2024-04-16 14:24:49 -0400

    first commit

---
+++ remove.py
@@ -0,0 +1,25 @@
+'''
+    This removes all the unecessary information about the bills to make the file easier to read
+
+    remove: BillingAccountName
+    BillingProfileName
+    AccountOwnerId
+    Date
+    ServiceInfo1
+    ServiceInfo2
+    ReservationId
+    ReservationName
+    benefitId
+    benefitName
+    ServiceFamily
+    CostAllocationRuleName
+    PublisherType
+    PartNumber
+    ProductOrderId
+    ProductOrderName
+    OfferId
+    CostCenter
+    ReservationId
+
+
+'''
