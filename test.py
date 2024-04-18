0a665476ad597176964a748ae4a62bcb9047bf9c
Kelly Do <kellydo8467@gmail.com>
2024-04-18 10:05:06 -0400

    finished rg comprarer and it writes into the excel

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ import csv
 import json
 import math
 
+def addDataColumn (wb, ws, path, dataList, columnNum, header):
+    cell = ws.cell(row = 1, column = columnNum)
+    cell.value = header
+    for index, value in enumerate(dataList, start=2):
+        cell = ws.cell(row=index, column=columnNum)
+        cell.value = value
+    wb.save(path)
 
 def applicationMerger(excelSheet, applications):
     sumDict = {}
@@ -21,21 +28,52 @@ def applicationMerger(excelSheet, applications):
             sumDict[value] += cost
     return sumDict
 
-if __name__ == "__main__":
-    excel = "c:\\Users\\do-kelly\\Downloads\\summary.xlsx"
-    sheet = "Summary"
+def rgComparer():
+    excel = "c:\\Users\\do-kelly\\Downloads\\help.xlsx"
+    comparison = pd.read_excel(excel, sheet_name = "RG Comparison")
+    lastMonthRg = comparison["Last Month RG"].tolist()
+    currentMonthRg = comparison["Current Month RG"].tolist()
 
-    summary = pd.read_excel(excel, sheet_name = sheet)
-    applications = summary.loc[2:221, "Unnamed: 0"]
+    print(comparison.columns)
+    deleted = []
+    added = []
+    for rgs in lastMonthRg:
+        if lastMonthRg not in currentMonthRg:
+            deleted.append(rgs)
+
+    for rg in currentMonthRg:
+        if currentMonthRg not in lastMonthRg:
+            added.append(rg)
+    data = {'Deleted' : deleted,
+            "Added" : added}
+    
+    dataFrame = pd.DataFrame(data)
+    
+    wb = load_workbook(excel)
+    sheet = wb["RG Comparison"]
+    addDataColumn(wb, sheet, excel, deleted, 4, "Deleted")
+    addDataColumn(wb, sheet, excel, added, 5, "Added")
+    # cell = sheet.cell(row = 1, column = 4)
+    # cell.value = "Deleted"
+    # for index, value in enumerate(deleted, start=2):
+    #     cell = sheet.cell(row=index, column=4)
+    #     cell.value = value
+
+    # wb.save(excel)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # excel = "c:\\Users\\do-kelly\\Downloads\\summary.xlsx"
+    # sheet = "Summary"
+
+    # summary = pd.read_excel(excel, sheet_name = sheet)
+    # applications = summary.loc[2:221, "Unnamed: 0"]
     # print(summary)
 
-    print(applicationMerger(summary, applications))
-
-
-
-    #for app in applications:
-
-    # testCol = test["Resource Group"]
-    # for value in testCol:
-    #     print(value)
-   
+    rgComparer()
+    # print(applicationMerger(summary, applications))
