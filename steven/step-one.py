f01047bbab7060dc15cd9a21fade197ed4a0f54b
Shannon <shannon-steven@aramark.com>
2024-04-18 16:17:15 -0400

    .

--- steven/step-one.py
+++ steven/step-one.py
@@ -1,40 +1,57 @@
 import pandas
 import openpyxl
-import csv, json
+import os
+import datetime, time
 
 if __name__ == "__main__":
-    billing_file_path = "../../hide/billing.csv"
+    #billing_file_path = None
+    #while not billing_file_path:
+    #    billing_file_path = input("Location of billing.csv:\t")
+    #    
+    #    if not os.path.exists(billing_file_path) or not os.access(billing_file_path, os.R_OK):
+    #        billing_file_path = None
 
-    df = pandas.read_csv(billing_file_path, na_values = "(blank)").filter(items = ["ResourceGroup", "Cost"])
-    df.fillna("(blank)", inplace = True)
-    df["RG_Lower"] = df["ResourceGroup"].str.lower()
+    #current_date = datetime.datetime.now()
+    #month = current_date.strftime("%B")
+    #year = current_date.strftime("%Y")
 
-    df = df.groupby("RG_Lower", as_index = False).agg({"ResourceGroup": "first", "Cost": "sum"})
-    df.drop(columns = "RG_Lower", inplace = True)
-    df.to_excel("../../hide/PivotTable_RG-Cost.xlsx", index = False)
+    billing_file_path = "../../hide/billing.xlsx"   ##############
+    month = "February"                              # TEMPORARY! #
+    year = "2024"                                   ##############
+
+    destination_path = billing_file_path
+
+    xl_wb = openpyxl.load_workbook(destination_path)
+    rg_cost_sheet = xl_wb.create_sheet("RG Comparison")
+    xl_wb.active = rg_cost_sheet
+
+    startt = time.time()
+    xl_wb.save(destination_path)
+    print(f"{time.time() - startt} to SAVE excel shett\n")
 
 
+    df = pandas.DataFrame(xl_wb["billing"].values)
 
-if __name__ == "__main9__":
-    billing_file_path = "../../hide/billing.csv"
+    print(df.head(5))
 
-    with open(billing_file_path, "r", encoding = "utf-8-sig") as csvf:
-        csvr = csv.DictReader(csvf)
+    df = df.filter(items = ["ResourceGroup", "Cost"])
 
-        infodump = {}
+    print(df.head(5))
 
-        for row in csvr:
-            row_k = row["ResourceGroup"].lower()
-            if not row_k: row_k = "(blank)"
+    #df.fillna("(blank)", inplace = True)
+    #df["RG_Lower"] = df["ResourceGroup"].str.lower()
+    #df["Last Month RG"] = df["ResourceGroup"]
+    #df["Current Month RG"] = df["ResourceGroup"]
 
-            if row_k not in infodump:
-                infodump[row_k] = "---"
+    #df = df.groupby("RG_Lower", as_index = False).agg({"Last Month RG": "first", "Current Month RG": "first", "Cost": "sum"})
+    #df.drop(columns = "RG_Lower", inplace = True)
 
-            if row["Tags"]:
-                asda = json.loads("{" + row["Tags"] + "}")
-                if "Profit Center" in asda.keys():
-                    infodump[row_k] = asda["Profit Center"]
+    #for row in df.head(10):
+    #    rg_cost_sheet.append(row)
+    #df.to_excel("../../hide/PivotTable_RG-Cost.xlsx", index = False)
 
-        with open(billing_file_path + "aads.txt", "w", encoding = "utf-8-sig") as writable:
-            for key, val in infodump.items():
-                writable.write(f"{key} -> {val}\n")
+    #with pandas.ExcelWriter(destination_path, engine = "openpyxl") as writer:
+    #    #writer.book = xl_wb
+    #   df.to_excel(writer, sheet_name="RG Comparison", index = False)
+    
+    xl_wb.close()
