85412d42fbb615a385b78562ac21cb4aa8cbccba
Kelly Do <kellydo8467@gmail.com>
2024-04-23 15:44:51 -0400

    finished chargeback for customer and tax

--- kelly/summary.py
+++ kelly/summary.py
@@ -4,7 +4,7 @@ import pandas as pd
 import math
 import time
 
-def addDataAndHeader (wb, ws, path, columnNum, header, width = 0, dataList = []):
+def addDataAndHeader (wb, pyxlWS, path, columnNum, header, width = 0, dataList = []):
     '''
     This adds all of the headers to the group summary sheet and their respective values
     @param wb is the workbook loaded using openpyxl
@@ -16,19 +16,19 @@ def addDataAndHeader (wb, ws, path, columnNum, header, width = 0, dataList = [])
     @param dataList is the list of the data you want in the column
     @return completed data and header
     '''
-    cell = ws.cell(row = 1, column = columnNum)
+    cell = pyxlWS.cell(row = 1, column = columnNum)
     cell.value = header
     cell.font = Font(bold=True)
     cell.alignment = Alignment(horizontal = "center")
 
     for index, value in enumerate(dataList, start=2):
-        cell = ws.cell(row=index, column=columnNum)
+        cell = pyxlWS.cell(row=index, column=columnNum)
         cell.value = value
         if (len(dataList) != 0 ) and (isinstance(dataList[0], float)):
             cell.style = "Currency"
 
     if width != 0:
-       ws.column_dimensions[chr(64 + columnNum)].width = width 
+       pyxlWS.column_dimensions[chr(64 + columnNum)].width = width 
 
     wb.save(path)
 
@@ -61,7 +61,7 @@ def merger(sheet, header1, header2 = "Group"):
     General merger to assign relationship between non cost categories
     @param sheet is the sheet read in by using pandas
     @param header1 is the header of the unique data you want to access
-    @param header2
+    @param header2 is either the Group or Applications
     @return a dictionary of the group and other dataset
     """
     colLength = len(sheet[header2]) - 1
@@ -76,8 +76,8 @@ def merger(sheet, header1, header2 = "Group"):
                 dict[value] = sheet.loc[index, header1]
                 uniqueApps.append(value)
     else:
-        for i, v in enumerate(applications, start = 0):
-            dict[v] = sheet.loc[i, header1]
+        for index, value in enumerate(applications, start = 0):
+            dict[value] = sheet.loc[index, header1]
 
     return dict
 
@@ -96,20 +96,6 @@ def infracharge(total):
     for i in range(length):
         data.append(cost)
     return data
-
-def addCharges(cost, infracharge):
-    """
-    Adds sales tax 3.05% and infracharge to the total costs
-    @param cost is the dictionary of groups and total costs
-    @param infracharge is the list of the infracharge
-    """
-    infra = infracharge[0]
-    for key in cost:
-        cost[key] += infra
-        taxes = float(cost[key] * .0305)
-        cost[key] += taxes
-    
-    return cost
 
 def creategroupSummarySheet(wb, sumSheet, path):
     """
@@ -135,9 +121,9 @@ def creategroupSummarySheet(wb, sumSheet, path):
     addDataAndHeader(wb, ws, path, 5, headers[5], 15.45, list(pc.values()))
     addDataAndHeader(wb, ws, path, 6, headers[6], 15.45, list(ac.values()))
 
-def createChargeback(wb, sumSheet, path):
+def createCustomerSheet(wb, sumSheet, path):
     """
-    Creates chargeback sheet for the month
+    Creates customer chargeback sheet for the month
     @param wb is the workbook opened by openpyxl
     @param sumsheet is the summary sheet opened by pandas
     @param path is the path to the excel workbook
@@ -151,10 +137,7 @@ def createChargeback(wb, sumSheet, path):
     for key in cost:
         cost[key] += charge[0]
 
-    # finalCosts = addCharges(cost, charge)
-
-    # finalCosts.pop("Infrastructure")
-    pc.pop("Infrastructure")
+    pc.pop("Infrastructure")        #find a way to shorten this
     ac.pop("Infrastructure")
     owner.pop("Infrastructure")
     cost.pop("Infrastructure")
@@ -166,23 +149,24 @@ def createChargeback(wb, sumSheet, path):
     addDataAndHeader(wb, ws, path, 1, headers[0], 31.64, list(owner.values()))
     addDataAndHeader(wb, ws, path, 2, headers[1], 32.64,  list(owner))
     addDataAndHeader(wb, ws, path, 3, headers[2], 19, list(cost.values()))
-    # addDataAndHeader(wb, ws, path, 3, headers[2], 19, list(finalCosts.values()))
     addDataAndHeader(wb, ws, path, 4, headers[3], 15.91, list(pc.values()))
     addDataAndHeader(wb, ws, path, 5, headers[4], 16.64, list(ac.values()))
 
+
 def createTaxSheet(wb, path, tax):
     """
+    Creates tax chargeback sheet for the month
     @param wb is the workbook opened by openpyxl
     @param path is a string of the path to the excel workbook
+    @param tax is a float of the tax percentage
     """
     customerSheet = wb["Customer Chargeback"]
     copy = wb.copy_worksheet(customerSheet)
     copy.title = "Tax Chargeback"
     wb.save(path)
 
-    ws = pd.read_excel(path, sheet_name="Tax Chargeback")
-    cost = merger(ws, "February (2024)", "Applications")
-
+    pandaWs = pd.read_excel(path, sheet_name="Tax Chargeback")
+    cost = merger(pandaWs, "February (2024)", "Applications")
     ws = wb["Tax Chargeback"]
 
     salesTax = []
@@ -190,16 +174,29 @@ def createTaxSheet(wb, path, tax):
 
     for i in range(len(cost)):
         taxPercent.append(tax)
-
     for key in cost:
         taxes = tax * cost[key]
         salesTax.append(taxes) 
         cost[key] += taxes
 
-
     addDataAndHeader(wb, ws, path, 6, "Sales tax %", 13.45, taxPercent)
     addDataAndHeader(wb, ws, path, 7, "Sales Tax", 13.55, salesTax)
     addDataAndHeader(wb, ws, path, 8, "Total", 16.82, list(cost.values()))
+    
+    wb.save(path)
 
+def createChargebacks(wb, path, sumSheet, tax):
+    """
+    Creates tax and customer chargeback sheet for the current month
+    @param wb is the worbook opened by openpyxl
+    @param path is a string of the path to the workbook
+    @param sumSheet is the summary sheet opened by pandas
+    @param tax is a float of the sales tax percentage
+    """
 
-    print(cost)
+    createCustomerSheet(wb, sumSheet, path)
+    createTaxSheet(wb, path, 0.0305)
+
+    pyxlWS = wb["Customer Chargeback"]
+    finalCosts = merger(pd.read_excel(path, "Tax Chargeback"), "Total", "Applications")
+    addDataAndHeader(wb, pyxlWS, path, 3, "February (2024)", 19,  list(finalCosts.values()))
